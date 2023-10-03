6936f78796abc15c4b5d91e4b6fba9e6289d3786
Jośe Júnior <josejuninho8@gmail.com>
2023-10-03 11:15:54 -0300

    Remoção de comentarios

--- collector/disks.go
+++ collector/disks.go
@@ -7,42 +7,31 @@ import (
 
 type disks struct {
 	//all labels are with location
-	meSession        *MeMetrics
-	slot             descMétrica
-	port             descMétrica
-	scsiID           descMétrica
-	blocksize        descMétrica
-	up               descMétrica // Serial number, vendor, model, revision
-	secondaryChannel descMétrica
-	//containerIndex   descMétrica
-	//memberIndex      descMétrica
-	description   descMétrica
-	architecture  descMétrica
-	diskInterface descMétrica
-	//singlePorted     descMétrica
-	diskType   descMétrica
-	usage      descMétrica
-	jobRunning descMétrica
-	//blink            descMétrica
-	//locatorLed       descMétrica
-	speed    descMétrica
-	smart    descMétrica
-	dualPort descMétrica
-	//error         descMétrica
-	driveDownCode descMétrica
-	owner         descMétrica
-	index         descMétrica
-	rpm           descMétrica
-	size          descMétrica
-	sectorFormat  descMétrica
-	transferRate  descMétrica
-	//attributes    descMétrica
-	//reconState descMétrica
-
-	// TODO: Testar no ME4
-	storageTier descMétrica //Me5?
-	ssdLifeLeft descMétrica //Me5?
-	//ledStatus            descMétrica //Me5?
+	meSession            *MeMetrics
+	slot                 descMétrica
+	port                 descMétrica
+	scsiID               descMétrica
+	blocksize            descMétrica
+	up                   descMétrica // Serial number, vendor, model, revision
+	secondaryChannel     descMétrica
+	description          descMétrica
+	architecture         descMétrica
+	diskInterface        descMétrica
+	diskType             descMétrica
+	usage                descMétrica
+	jobRunning           descMétrica
+	speed                descMétrica
+	smart                descMétrica
+	dualPort             descMétrica
+	driveDownCode        descMétrica
+	owner                descMétrica
+	index                descMétrica
+	rpm                  descMétrica
+	size                 descMétrica
+	sectorFormat         descMétrica
+	transferRate         descMétrica
+	storageTier          descMétrica
+	ssdLifeLeft          descMétrica
 	diskDsdCount         descMétrica
 	spunDown             descMétrica
 	IOCount              descMétrica
@@ -56,9 +45,6 @@ type disks struct {
 	extendedStatus       descMétrica
 	health               descMétrica
 	logger               log.Logger
-	//FcPChannel                  descMétrica //Fc P1 and P2
-	//FcPDeviceID                 descMétrica //Fc P1 and P2
-	//FcPUnitNumber               descMétrica //Fc P1 and P2
 }
 
 func identifyCodes(mask uint64) map[uint64]string {
@@ -147,16 +133,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				"Up of the disk", []string{"disk", "vendor", "model",
 					"virtual_disk", "disk_group", "storage_pool"}),
 		},
-		//containerIndex: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "container_index"),
-		//		"Container index of the disk", []string{"disk"}),
-		//},
-		//memberIndex: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "member_index"),
-		//		"Member index of the disk", []string{"disk"}),
-		//},
 		description: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("disks", "description"),
@@ -172,11 +148,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				NomeMetrica("disks", "interface"),
 				"Disk interface type", []string{"disk"}),
 		},
-		//singlePorted: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "single_ported"),
-		//		"Is disk single ported ?", []string{"disk"}),
-		//},
 		diskType: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("disks", "type"),
@@ -192,16 +163,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				NomeMetrica("disks", "job_running"),
 				"Job running of the disk", []string{"disk", "job_running", "status"}),
 		},
-		//blink: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "blink"),
-		//		"Blink of the disk", []string{"disk"}),
-		//},
-		//locatorLed: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "locator_led"),
-		//		"Locator led of the disk", []string{"disk"}),
-		//},
 		speed: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("disks", "speed"),
@@ -217,11 +178,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				NomeMetrica("disks", "dual_port"),
 				"Dual port of the disk", []string{"disk"}),
 		},
-		//error: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "error"),
-		//		"Error of the disk", []string{"disk"}),
-		//},
 		driveDownCode: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("disks", "drive_down_code"),
@@ -257,17 +213,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				NomeMetrica("disks", "transfer_rate"),
 				"Transfer rate of the disk", []string{"disk", "rate"}),
 		},
-		//attributes: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "attributes"),
-		//		"Attributes of the disk", []string{"disk", "attributes"}),
-		//},
-		//reconState: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "recon_state"),
-		//		"Recon state of the disk", []string{"disk", "recon_state"}),
-		//},
-
 		storageTier: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("disks", "storage_tier"),
@@ -278,11 +223,6 @@ func NewDisks(me *MeMetrics, logger log.Logger) (Coletor, error) {
 				NomeMetrica("disks", "ssd_life_left"),
 				"Ssd life left of the disk", []string{"disk", "percentage_left"}),
 		},
-		//ledStatus: descMétrica{prometheus.GaugeValue,
-		//	NewDescritor(
-		//		NomeMetrica("disks", "led_status"),
-		//		"Led status of the disk", []string{"disk", "led_status"}),
-		//},
 		diskDsdCount: descMétrica{prometheus.CounterValue,
 			NewDescritor(
 				NomeMetrica("disks", "disk_dsd_count"),
@@ -360,22 +300,15 @@ func (d disks) Update(ch chan<- prometheus.Metric) error {
 		ch <- prometheus.MustNewConstMetric(d.scsiID.desc, d.scsiID.tipo, float64(disk.ScsiID), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.secondaryChannel.desc, d.secondaryChannel.tipo, float64(disk.SecondaryChannel), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.blocksize.desc, d.blocksize.tipo, float64(disk.Blocksize), disk.Location)
-		//ch <- prometheus.MustNewConstMetric(d.containerIndex.desc, d.containerIndex.tipo, float64(disk.ContainerIndex), disk.Location)
-		//ch <- prometheus.MustNewConstMetric(d.memberIndex.desc, d.memberIndex.tipo, float64(disk.MemberIndex), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.description.desc, d.description.tipo, float64(disk.DescriptionNumeric), disk.Location, disk.Description)
 		ch <- prometheus.MustNewConstMetric(d.architecture.desc, d.architecture.tipo, float64(disk.ArchitectureNumeric), disk.Location, disk.Architecture)
 		ch <- prometheus.MustNewConstMetric(d.diskInterface.desc, d.diskInterface.tipo, float64(disk.InterfaceNumeric), disk.Location)
-		//ch <- prometheus.MustNewConstMetric(d.singlePorted.desc, d.singlePorted.tipo, float64(disk.SinglePortedNumeric), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.diskType.desc, d.diskType.tipo, float64(disk.TypeNumeric), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.usage.desc, d.usage.tipo, float64(disk.UsageNumeric), disk.Location, disk.Usage)
 		ch <- prometheus.MustNewConstMetric(d.jobRunning.desc, d.jobRunning.tipo, float64(disk.JobRunningNumeric), disk.Location, disk.JobRunning, disk.CurrentJobCompletion)
-		//ch <- prometheus.MustNewConstMetric(d.blink.desc, d.blink.tipo, float64(disk.Blink), disk.Location)
-		//ch <- prometheus.MustNewConstMetric(d.locatorLed.desc, d.locatorLed.tipo, float64(disk.LocatorLedNumeric), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.speed.desc, d.speed.tipo, float64(disk.Speed), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.smart.desc, d.smart.tipo, float64(disk.SmartNumeric), disk.Location, disk.Smart)
 		ch <- prometheus.MustNewConstMetric(d.dualPort.desc, d.dualPort.tipo, float64(disk.DualPort), disk.Location)
-		// TODO: Incluir métricas FcP1Channel, FcP1DeviceID, FcP1NodeWwn, FcP1PortWwn, FcP1UnitNumber
-		//ch <- prometheus.MustNewConstMetric(d.error.desc, d.error.tipo, float64(disk.Error), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.driveDownCode.desc, d.driveDownCode.tipo, float64(disk.DriveDownCode), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.owner.desc, d.owner.tipo, float64(disk.OwnerNumeric), disk.Location, disk.Owner)
 		ch <- prometheus.MustNewConstMetric(d.index.desc, d.index.tipo, float64(disk.Index), disk.Location)
@@ -383,12 +316,8 @@ func (d disks) Update(ch chan<- prometheus.Metric) error {
 		ch <- prometheus.MustNewConstMetric(d.size.desc, d.size.tipo, float64(disk.Blocks), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.sectorFormat.desc, d.sectorFormat.tipo, float64(disk.SectorFormatNumeric), disk.Location, disk.SectorFormat)
 		ch <- prometheus.MustNewConstMetric(d.transferRate.desc, d.transferRate.tipo, float64(disk.Speed), disk.Location, disk.TransferRate)
-		//ch <- prometheus.MustNewConstMetric(d.attributes.desc, d.attributes.tipo, float64(disk.AttributesNumeric), disk.Location, disk.Attributes)
-		//ch <- prometheus.MustNewConstMetric(d.reconState.desc, d.reconState.tipo, float64(disk.ReconStateNumeric), disk.Location, disk.ReconState)
 		ch <- prometheus.MustNewConstMetric(d.storageTier.desc, d.storageTier.tipo, float64(disk.StorageTierNumeric), disk.Location, disk.StorageTier)
 		ch <- prometheus.MustNewConstMetric(d.ssdLifeLeft.desc, d.ssdLifeLeft.tipo, float64(disk.SsdLifeLeftNumeric), disk.Location, disk.SsdLifeLeft)
-		//ch <- prometheus.MustNewConstMetric(d.ledStatus.desc, d.ledStatus.tipo, float64(disk.LedStatusNumeric), disk.Location, disk.LedStatus)
-
 		ch <- prometheus.MustNewConstMetric(d.diskDsdCount.desc, d.diskDsdCount.tipo, float64(disk.DiskDsdCount), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.spunDown.desc, d.spunDown.tipo, float64(disk.SpunDown), disk.Location)
 		ch <- prometheus.MustNewConstMetric(d.IOCount.desc, d.IOCount.tipo, float64(disk.NumberOfIos), disk.Location)
