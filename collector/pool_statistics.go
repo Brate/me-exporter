175016eec10d93cfccb091d7f7695bcb484ac232
Fernando Braga <fernando@brate.com.br>
2023-10-20 10:36:55 -0300

    Pool statistics revision

--- collector/pool_statistics.go
+++ collector/pool_statistics.go
@@ -7,30 +7,18 @@ import (
 )
 
 type poolStatistics struct {
-	//Where have perMinute or PerHour is prometheus.GaugeValue
-	meSession                        *MeMetrics
-	sampleTime                       descMétrica
-	pagesAllocPerMinute              descMétrica
-	pagesAllocPerHour                descMétrica
-	pagesDeallocPerMinute            descMétrica
-	pagesDeallocPerHour              descMétrica
-	pagesUnmapPerMinute              descMétrica
-	pagesUnmapPerHour                descMétrica
-	numBlockedSsdPromotionsPerMinute descMétrica
-	numBlockedSsdPromotionsPerHour   descMétrica
-	numPageAllocations               descMétrica
-	numPageDeallocations             descMétrica
-	numPageUnmaps                    descMétrica
-	numPagePromotionsToSsdBlocked    descMétrica
-	numHotPageMoves                  descMétrica
-	numColdPageMoves                 descMétrica
+	meSession                     *MeMetrics
+	numPageAllocations            descMétrica
+	numPageDeallocations          descMétrica
+	numPageUnmaps                 descMétrica
+	numPagePromotionsToSsdBlocked descMétrica
+	numHotPageMoves               descMétrica
+	numColdPageMoves              descMétrica
 
 	// TierStatistics
-	Tier                      descMétrica
-	tierPagesAllocPerMinute   descMétrica
-	tierPagesDeallocPerMinute descMétrica
-	pagesReclaimed            descMétrica
-	numPagesUnmapPerMinute    descMétrica
+	// TODO: Add Response time metrics on TierStatistics
+	Tier           descMétrica
+	pagesReclaimed descMétrica
 
 	logger log.Logger
 }
@@ -42,107 +30,47 @@ func init() {
 func NewPoolStatisticsCollector(me *MeMetrics, logger log.Logger) (Coletor, error) {
 	return &poolStatistics{
 		meSession: me,
-		sampleTime: descMétrica{prometheus.GaugeValue,
+		numPageAllocations: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "sample_time"),
-				"Sample time", []string{"pool", "sample_time"}),
-		},
-		pagesAllocPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_alloc_per_minute"),
-				"Pages alloc per minute", []string{"pool"}),
-		},
-		pagesAllocPerHour: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_alloc_per_hour"),
-				"Pages alloc per hour", []string{"pool"}),
-		},
-		pagesDeallocPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_dealloc_per_minute"),
-				"Pages dealloc per minute", []string{"pool"}),
-		},
-		pagesDeallocPerHour: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_dealloc_per_hour"),
-				"Pages dealloc per hour", []string{"pool"}),
-		},
-		pagesUnmapPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_unmap_per_minute"),
-				"Pages unmap per minute", []string{"pool"}),
-		},
-		pagesUnmapPerHour: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "pages_unmap_per_hour"),
-				"Pages unmap per hour", []string{"pool"}),
-		},
-		numBlockedSsdPromotionsPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "num_blocked_ssd_promotions_per_minute"),
-				"Num blocked ssd promotions per minute", []string{"pool"}),
-		},
-		numBlockedSsdPromotionsPerHour: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "num_blocked_ssd_promotions_per_hour"),
-				"Num blocked ssd promotions per hour", []string{"pool"}),
-		},
-		numPageAllocations: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "num_page_allocations"),
+				NomeMetrica("pool_statistics", "page_allocations"),
 				"Num page allocations", []string{"pool"}),
 		},
-		numPageDeallocations: descMétrica{prometheus.GaugeValue,
+		numPageDeallocations: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "num_page_deallocations"),
+				NomeMetrica("pool_statistics", "page_deallocations"),
 				"Num page deallocations", []string{"pool"}),
 		},
-		numPageUnmaps: descMétrica{prometheus.GaugeValue,
+		numPageUnmaps: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "num_page_unmaps"),
+				NomeMetrica("pool_statistics", "page_unmaps"),
 				"Num page unmaps", []string{"pool"}),
 		},
-		numPagePromotionsToSsdBlocked: descMétrica{prometheus.GaugeValue,
+		numPagePromotionsToSsdBlocked: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "num_page_promotions_to_ssd_blocked"),
+				NomeMetrica("pool_statistics", "blocked_ssd_page_promotions"),
 				"Num page promotions to ssd blocked", []string{"pool"}),
 		},
-		numHotPageMoves: descMétrica{prometheus.GaugeValue,
+		numHotPageMoves: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "num_hot_page_moves"),
+				NomeMetrica("pool_statistics", "hot_page_moves"),
 				"Num hot page moves", []string{"pool"}),
 		},
-		numColdPageMoves: descMétrica{prometheus.GaugeValue,
+		numColdPageMoves: descMétrica{prometheus.CounterValue,
 			NewDescritor(
-				NomeMetrica("pool_statistics", "num_cold_page_moves"),
+				NomeMetrica("pool_statistics", "cold_page_moves"),
 				"Num cold page moves", []string{"pool"}),
 		},
-		//TierStatistics
 
+		// TierStatistics
 		Tier: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("pool_statistics", "tier"),
 				"Tier", []string{"pool", "tier"}),
 		},
-		tierPagesAllocPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "tier_pages_alloc_per_minute"),
-				"Tier pages alloc per minute", []string{"pool", "tier"}),
-		},
-		tierPagesDeallocPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "tier_pages_dealloc_per_minute"),
-				"Tier pages dealloc per minute", []string{"pool", "tier"}),
-		},
 		pagesReclaimed: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("pool_statistics", "pages_reclaimed"),
 				"Pages reclaimed", []string{"pool", "tier"}),
-		},
-		numPagesUnmapPerMinute: descMétrica{prometheus.GaugeValue,
-			NewDescritor(
-				NomeMetrica("pool_statistics", "num_pages_unmap_per_minute"),
-				"Num pages unmap per minute", []string{"pool", "tier"}),
 		},
 		logger: logger,
 	}, nil
@@ -154,15 +82,6 @@ func (p poolStatistics) Update(ch chan<- prometheus.Metric) error {
 	}
 
 	for _, pool := range p.meSession.poolStatistics {
-		ch <- p.sampleTime.constMetric(float64(pool.SampleTimeNumeric), pool.Pool, pool.SampleTime)
-		ch <- p.pagesAllocPerMinute.constMetric(float64(pool.PagesAllocPerMinute), pool.Pool)
-		ch <- p.pagesAllocPerHour.constMetric(float64(pool.PagesAllocPerHour), pool.Pool)
-		ch <- p.pagesDeallocPerMinute.constMetric(float64(pool.PagesDeallocPerMinute), pool.Pool)
-		ch <- p.pagesDeallocPerHour.constMetric(float64(pool.PagesDeallocPerHour), pool.Pool)
-		ch <- p.pagesUnmapPerMinute.constMetric(float64(pool.PagesUnmapPerMinute), pool.Pool)
-		ch <- p.pagesUnmapPerHour.constMetric(float64(pool.PagesUnmapPerHour), pool.Pool)
-		ch <- p.numBlockedSsdPromotionsPerMinute.constMetric(float64(pool.NumBlockedSsdPromotionsPerMinute), pool.Pool)
-		ch <- p.numBlockedSsdPromotionsPerHour.constMetric(float64(pool.NumBlockedSsdPromotionsPerHour), pool.Pool)
 		ch <- p.numPageAllocations.constMetric(float64(pool.NumPageAllocations), pool.Pool)
 		ch <- p.numPageDeallocations.constMetric(float64(pool.NumPageDeallocations), pool.Pool)
 		ch <- p.numPageUnmaps.constMetric(float64(pool.NumPageUnmaps), pool.Pool)
@@ -180,9 +99,6 @@ func (p poolStatistics) Update(ch chan<- prometheus.Metric) error {
 func (p poolStatistics) collectTierStatistics(ch chan<- prometheus.Metric, pool Me.PoolStatistics) {
 	for _, tier := range pool.TierStatistics {
 		ch <- p.Tier.constMetric(float64(tier.TierNumeric), pool.Pool, tier.Tier)
-		ch <- p.tierPagesAllocPerMinute.constMetric(float64(tier.PagesAllocPerMinute), pool.Pool, tier.Tier)
-		ch <- p.tierPagesDeallocPerMinute.constMetric(float64(tier.PagesDeallocPerMinute), pool.Pool, tier.Tier)
 		ch <- p.pagesReclaimed.constMetric(float64(tier.PagesReclaimed), pool.Pool, tier.Tier)
-		ch <- p.numPagesUnmapPerMinute.constMetric(float64(tier.NumPagesUnmapPerMinute), pool.Pool, tier.Tier)
 	}
 }
