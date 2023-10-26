99f15e16af1e9a7240d2661cbfad51f968476a13
Jośe Júnior <josejuninho8@gmail.com>
2023-10-26 09:39:40 -0300

    Criação do collector do Tier

--- collector/tier_statistics.go
+++ collector/tier_statistics.go
@@ -8,11 +8,11 @@ import (
 type tierStatistics struct {
 	meSession              *MeMetrics
 	up                     descMétrica
-	TierNumeric            descMétrica
-	PagesAllocPerMinute    descMétrica
-	PagesDeallocPerMinute  descMétrica
-	PagesReclaimed         descMétrica
-	NumPagesUnmapPerMinute descMétrica
+	tierNumeric            descMétrica
+	pagesAllocPerMinute    descMétrica
+	pagesDeallocPerMinute  descMétrica
+	pagesReclaimed         descMétrica
+	numPagesUnmapPerMinute descMétrica
 	logger                 log.Logger
 }
 
@@ -28,27 +28,27 @@ func NewTierStatisticsCollector(me *MeMetrics, logger log.Logger) (Coletor, erro
 				NomeMetrica("tier_statistics", "up"),
 				"Was the last query of tier_statistics successful.", []string{"pool", "serial_number"}),
 		},
-		TierNumeric: descMétrica{prometheus.GaugeValue,
+		tierNumeric: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("tier_statistics", "tier_numeric"),
 				"Tier numeric", []string{"pool", "num_tier"}),
 		},
-		PagesAllocPerMinute: descMétrica{prometheus.GaugeValue,
+		pagesAllocPerMinute: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("tier_statistics", "pages_alloc_per_minute"),
 				"Pages alloc per minute", []string{"pool"}),
 		},
-		PagesDeallocPerMinute: descMétrica{prometheus.GaugeValue,
+		pagesDeallocPerMinute: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("tier_statistics", "pages_dealloc_per_minute"),
 				"Pages dealloc per minute", []string{"pool"}),
 		},
-		PagesReclaimed: descMétrica{prometheus.GaugeValue,
+		pagesReclaimed: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("tier_statistics", "pages_reclaimed"),
 				"Pages reclaimed", []string{"pool"}),
 		},
-		NumPagesUnmapPerMinute: descMétrica{prometheus.GaugeValue,
+		numPagesUnmapPerMinute: descMétrica{prometheus.GaugeValue,
 			NewDescritor(
 				NomeMetrica("tier_statistics", "num_pages_unmap_per_minute"),
 				"Num pages unmap per minute", []string{"pool"}),
@@ -62,13 +62,13 @@ func (t tierStatistics) Update(ch chan<- prometheus.Metric) error {
 		return err
 	}
 
-	for _, tier := range t.meSession.tierStatistics {
-		ch <- t.up.constMetric(1, tier.Pool, tier.Tier, tier.SerialNumber)
-		ch <- t.TierNumeric.constMetric(float64(tier.TierNumeric), tier.Pool, tier.Tier)
-		ch <- t.PagesAllocPerMinute.constMetric(float64(tier.PagesAllocPerMinute), tier.Pool)
-		ch <- t.PagesDeallocPerMinute.constMetric(float64(tier.PagesDeallocPerMinute), tier.Pool)
-		ch <- t.PagesReclaimed.constMetric(float64(tier.PagesReclaimed), tier.Pool)
-		ch <- t.NumPagesUnmapPerMinute.constMetric(float64(tier.NumPagesUnmapPerMinute), tier.Pool)
+	for _, tierS := range t.meSession.tierStatistics {
+		ch <- t.up.constMetric(1, tierS.Pool, tierS.Tier, tierS.SerialNumber)
+		ch <- t.tierNumeric.constMetric(float64(tierS.TierNumeric), tierS.Pool, tierS.Tier)
+		ch <- t.pagesAllocPerMinute.constMetric(float64(tierS.PagesAllocPerMinute), tierS.Pool)
+		ch <- t.pagesDeallocPerMinute.constMetric(float64(tierS.PagesDeallocPerMinute), tierS.Pool)
+		ch <- t.pagesReclaimed.constMetric(float64(tierS.PagesReclaimed), tierS.Pool)
+		ch <- t.numPagesUnmapPerMinute.constMetric(float64(tierS.NumPagesUnmapPerMinute), tierS.Pool)
 	}
 
 	return nil
